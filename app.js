8eb965197e03da35cfde826eab09d54e866596e3
Moutlou <moutlou@outlook.com>
2019-04-24 15:41:24 +0300

    general data added , tabs component corrected

--- app.js
+++ app.js
@@ -9,16 +9,34 @@ app.set('view engine', 'ejs');
 const ROOT_DIR = __dirname;
 app.use(express.static(path.join(ROOT_DIR + '/public')));
 
-router.get('/' ,(req,res) => {       
+router.get('/' ,(req,res) => {     
+    showInfo(res , loadPage);
+});
+
+showInfo = (res ,next) => {
+
+    var generalInfo  = new Object({
+        operatingSystem : os.type(),
+        platform : os.platform(),
+        hostname : os.hostname(),
+        freeMem : os.freemem(),
+        totalMem : os.totalmem(),
+        user : os.userInfo()
+        
+    });
     var data = new Object ({
+        generalInfo: Object.entries(generalInfo),
         constants: Object.entries(os.constants) ,
         cpus : Object.entries(os.cpus()),
         networkInterfaces : Object.entries(os.networkInterfaces())
-    });    
+    });   
+        
+    next(res,data);
+}
 
-    console.log(Object.entries(os.networkInterfaces()));
+loadPage = (res, data) => {
     res.render('pages/index' , { data: data } );
-});
+}
 
 
 app.use(router);
